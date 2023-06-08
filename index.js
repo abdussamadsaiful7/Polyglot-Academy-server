29afb5beb5bdaf24e03e42e5e3bd938c886a9097
ABDUS SAMAD SAIFUL <samadsaifullah11@gmail.com>
2023-06-09 04:14:30 +0600

    added user api

--- index.js
+++ index.js
@@ -42,6 +42,11 @@ async function run() {
         //users api
         app.post('/users', async (req, res)=>{
             const user = req.body;
+            const query = {email: user.email}
+            const existingUser = await userCollection.findOne(query);
+            if(existingUser){
+                return res.send({ message: 'user already exists'})
+            }
             const result = await userCollection.insertOne(user);
             res.send(result);
         })
